279720dad84905d7ad6a8df3a1bc9669555ea781
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2019-09-20 17:36:42 +0800

    Add cards and expansions model

--- index/models.py
+++ index/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Card(models.Model):
+    # Card ID
+    card_id = models.IntegerField(primary_key=True)
+    # Class
+    clan = models.IntegerField()
+    # Name
+    card_name = models.CharField(max_length=64)
+    # Rarity
+    rarity = models.IntegerField()
+    # Expansion
+    card_set_id = models.IntegerField()
+
+class Expansion(models.Model):
+    # Expansion
+    card_set_id = models.IntegerField()
+    # Expansion Name
+    expac_name = models.CharField(max_length=64)
