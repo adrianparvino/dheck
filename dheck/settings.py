1dc0da0505abf869793c43253453613deb7f84ce
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2019-09-22 20:27:18 +0800

    Allow any hostname

--- dheck/settings.py
+++ dheck/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'gzaec*$mcrl_3_xb+pt10z-t++ll!)uxc94ft0y0bgxt+n0m(-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
